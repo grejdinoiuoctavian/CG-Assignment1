5154e28024808cfecb0cbed0403b49a609ab7108
grejdinoiuoctavian <56017060+grejdinoiuoctavian@users.noreply.github.com>
2022-11-18 17:24:08 +0100

    Button manager allows player to swap between scenes properly

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,18 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField]
+    public Button Store_btn, Game_btn, Back_ss_btn, Back_gs_btn, Exit_btn;
     // Start is called before the first frame update
     void Start()
     {
+        try
+        {
+            Store_btn.onClick.AddListener(StartStore);
+        }
+        catch(System.NullReferenceException)
+        {
+            Debug.Log("Some buttons are missing from the scene because they are not required");
+        }
         
+        try
+        {
+            Game_btn.onClick.AddListener(StartGame);
+        }
+        catch(System.NullReferenceException)
+        {
+            Debug.Log("Some buttons are missing from the scene because they are not required");
+        }
+        
+        try
+        {
+            Back_ss_btn.onClick.AddListener(BackStore);
+        }
+        catch(System.NullReferenceException)
+        {
+            Debug.Log("Some buttons are missing from the scene because they are not required");
+        }
+        
+        try
+        {
+            Back_gs_btn.onClick.AddListener(BackGame);
+        }
+        catch(System.NullReferenceException)
+        {
+            Debug.Log("Some buttons are missing from the scene because they are not required");
+        }
+        
+        try
+        {
+            Exit_btn.onClick.AddListener(ExitGame);
+        }
+        catch(System.NullReferenceException)
+        {
+            Debug.Log("Some buttons are missing from the scene because they are not required");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void StartStore()
     {
-        
+        SceneManager.LoadScene("Store_Scene");
+    }
+    
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Game_Scene");
+    }
+    
+    private void BackStore()
+    {
+        SceneManager.LoadScene("Welcome_Scene");
+    }
+    
+    private void BackGame()
+    {
+        SceneManager.LoadScene("Welcome_Scene");
+    }
+    
+    private void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
